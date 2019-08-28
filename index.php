21107f1d0a29c5c2f2bed06bddfc659eb085aa09
vorabrijesh <cs17b031@iittp.ac.in>
2019-08-28 16:13:41 +0530

    php integration

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
-            echo "Hi, I'm a PHP script!";
-        ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+     <script type="text/javascript" src="script.js"></script>
+    <title>Agriculure solutions</title>
+  </head>
+  <body>
+    <h1>Weather api</h1>
+   
+    <div class="weather-container">
+      <img class="icon"> 
+      <p class="weather"></p>
+      <p class="temp">Temperature = </p>
+      <p class="pressure">Pressure = </p>
+      <p class="humid">Humid = </p>
+      <p class="desc">Description = </p>
+    </div>
+
+    <?php
+    echo "Hello world";
+    ?>
+  
+
+  </body>
+</html>
